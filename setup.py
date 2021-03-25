569641a14676b0773d0ee4a21b295e26be64a859
whitetuft <salvatore.larosa@imaa.cnr.it>
2021-03-25 11:20:53 +0100

    add minimal setup with requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyrtlib',
+    version='1.0.0',
+    packages=['pyrtlib'],
+    url='',
+    license='MIT License',
+    author='slarosa',
+    author_email='salvatore.larosa@imaa.cnr.it',
+    description='pyrtlib - Radiative Transfer library'
+)
