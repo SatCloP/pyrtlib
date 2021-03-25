e945f09f20ca8618fb301db9c00bb43a401b7124
whitetuft <salvatore.larosa@imaa.cnr.it>
2021-03-25 11:21:15 +0100

    followup: add missing files

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='pyrtlib',
     version='1.0.0',
     packages=['pyrtlib'],
+    install_requires=required,
     url='',
     license='MIT License',
     author='slarosa',
