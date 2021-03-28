ef8dbc5f83020780a4457d646b8db1efcabd4389
Salvatore Larosa <lrssvtml@gmail.com>
2021-03-28 18:47:46 +0200

    add test for tb_cloud with rose19sd

---
+++ pyrtlib/tests/test_main.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+import os
+import numpy as np
+from numpy.testing import assert_allclose
+import pandas as pd
+from pyrtlib.atmp import AtmosphericProfiles as atmp
+from pyrtlib.main import tb_cloud_rte
+from pyrtlib.utils import ppmv2gkg, mr2rh
+
+
+class Test(TestCase):
+    def test_tb_cloud_rte_with_rose19sd(self):
+        z, p, d, t, md = atmp.gl_atm(atmp.TROPICAL)
+
+        gkg = ppmv2gkg(md[:, 0], atmp.H20)
+        rh = mr2rh(p, t, gkg)[0] / 100
+
+        ang = np.array([90.])
+        frq = np.arange(20, 201, 1)
+        nf = len(frq)
+
+        denliq = np.zeros(z.shape)
+        denice = np.zeros(z.shape)
+        cldh = np.zeros((2, 0))
+
+        df = tb_cloud_rte(z, p, t, rh, denliq, denice, cldh, frq, ang, absmdl='rose19sd', ray_tracing=True,
+                          from_sat=True)
+        df = df.set_index(frq)
+        df_expected = pd.read_csv("tests/data/test_rose19sd_tb.csv")
+        assert_allclose(df.tbtotal, df_expected.tbtotal, atol=0)
