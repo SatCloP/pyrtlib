9300934a79295b963f93ba91bbdb4b60af821e5a
slarosa <lrssvtml@gmail.com>
2023-04-27 09:19:30 +0200

    install missing depends in order to fix build doc

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -5,9 +5,9 @@ set -x
 ###################
 
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-git python3-pip python3-virtualenv python3-setuptools pandoc
+apt-get -y install git rsync python3-sphinx python3-git python3-pip python3-virtualenv python3-setuptools pandoc zlib1g
 python3 -m pip install --upgrade rinohtype pygments
-pip3 install sphinx-toggleprompt nbconvert==5.6.1 nbsphinx sphinx-gallery sphinx-copybutton sphinx-toggleprompt sphinx-panels pandas matplotlib
+pip3 install pydata-sphinx-theme sphinx-toggleprompt nbconvert==5.6.1 nbsphinx sphinx-gallery sphinx-copybutton sphinx-toggleprompt sphinx-panels pandas matplotlib
 
 #####################
 # DECLARE VARIABLES #
