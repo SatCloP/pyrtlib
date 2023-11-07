8f102883b75eca81bfd19c9395568d1c6839a53b
whitetuft <salvatore.larosa@imaa.cnr.it>
2023-11-07 19:54:56 +0100

    add more unit test for extrapolation

--- pyrtlib/tests/test_extrapolation.py
+++ pyrtlib/tests/test_extrapolation.py
@@ -2,6 +2,7 @@ import os
 # from pathlib import Path
 from unittest import TestCase
 from datetime import datetime
+from numpy.testing import assert_allclose
 from pyrtlib.climatology import ProfileExtrapolation
 from pyrtlib.apiwebservices import IGRAUpperAir
 from pyrtlib.utils import dewpoint2rh, to_kelvin
@@ -31,3 +32,18 @@ class Test(TestCase):
             header.latitude.values[0], 6, z, (p, t, rh))
         assert len(zz) > 25
         assert min(pp) < 10
+        
+    def test_standard_temp(self):
+        ex = ProfileExtrapolation()
+        t = ex.standard_temperature(12, 280)
+        assert_allclose(t, 216.65)
+        
+    def test_standard_pres(self):
+        ex = ProfileExtrapolation()
+        p = ex.standard_pressure(12, 280, 1013)
+        assert_allclose(p, 193.99616049)
+
+    def test_standard_wv(self):
+        ex = ProfileExtrapolation()
+        wv = ex.standard_water_vapour_pressure(12)
+        assert_allclose(wv, 0.018586351836920856)
